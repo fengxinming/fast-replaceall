a787b37e3bf87769bd715ed528488cfef0f487f5
Jesse Feng <fxm0016@126.com>
2021-05-04 13:41:34 +0800

    Create replaceAll.test.js

---
+++ benchmark/replaceAll.test.js
@@ -0,0 +1,14 @@
+const replaceAll = require('../dist/replaceAll');
+
+// 测试 max
+const str = 'The quick brown fox jumps over the lazy dog. If the dog reacted, was it really lazy?';
+
+module.exports = {
+  '【String.prototype.replace】': function () {
+    str.replace(/dog/g, 'monkey');
+  },
+
+  '【replaceAll】': function () {
+    replaceAll(str, 'monkey');
+  },
+};
